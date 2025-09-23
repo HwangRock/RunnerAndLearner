a7ff4474f100e3dcdbc65ca406a08d9b0a6b87f7
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-09-24 00:44:54 +0900

    feat: 러닝, 비러닝 DTO 정의

---
+++ server/dto/ex_running_dto.py
@@ -0,0 +1,4 @@
+class ExRunningDto:
+    def __init__(self,date:str, time:int):
+        self.date=date
+        self.time=time
---
+++ server/dto/runnning_dto.py
@@ -0,0 +1,6 @@
+class RunningDto:
+    def __init__(self, date: str, time: int, velocity: float, distance: float):
+        self.date = date
+        self.time = time
+        self.velocity = velocity
+        self.distance = distance
